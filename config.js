6c8c3792d92bd5338d4ac18c8216eca1c4a9c51d
Khailanab <khailanabdala15@gmail.com>
2026-08-20 16:22:47 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Renomeie este arquivo para config.js e preencha com os dados do seu projeto Supabase.
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://SEU-PROJETO.supabase.co",
+  SUPABASE_URL: "https://toqvgwdbwxzyaepsuixl.supabase.co/rest/v1/",
   SUPABASE_ANON_KEY: "SUA-CHAVE-ANON-PUBLICA",
   APP_NAME: "Arquivo & Prazos"
 };
